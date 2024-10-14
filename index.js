6e73ba4f74f5e22347fdaa66609bbbdd7898d89c
Kelly Paxton <klypxtn@gmail.com>
2024-10-13 23:48:55 -0600

    git init

---
+++ index.js
@@ -0,0 +1,44 @@
+const cities = {
+    'Seattle': [150, 25],
+    'SanFrancisco': [65, 280],
+    'LosAngeles': [],
+    'LasVegas': [],
+    'Boise': [],
+    'Pheonix': [],
+    'Albuquerque': [],
+    'Denver': [480, 280],
+    'Dallas': [],
+    'SanAntonio': [],
+    'Houston': [],
+    'Minnieapolis': [],
+    'KansasCity': [],
+    'SaintLouis': [],
+    'NewOrleans': [],
+    'Nashville': [],
+    'Springfeild': [],
+    'Chicago': [],
+    'Atlanta': [],
+    'Jacksonville': [],
+    'Miami': [],
+    'Charleston': [],
+    'Cincinnati': [],
+    'Detroit': [],
+    'Cleveland': [],
+    'Richmond': [],
+    'Pittsburg': [],
+    'Buffalo': [],
+    'Centralia': [],
+    'Boston': [],
+    'Providence': [],
+    'NewYork': [],
+    'Philadelphia': [],
+    'Sparks': [],
+    'Baltimore': [],
+    'Washington': [],
+};
+
+const drone = document.getElementById("drone");
+
+function viewDeliveries() {
+        drone.style.transform = 'translate(330px, 255px)'; 
+}
